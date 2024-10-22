528e1e7e0d7dc027f397b6b672807da5fd700236
dewei-wang19 <dewei@bu.edu>
2024-10-22 19:43:33 -0400

    remote latest frontend templete

--- cs601_DeweiWang_term_project/src/utils/request.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
-// Creating an axios instance
-const service = axios.create({
-  timeout: 100000
-})
-
-export default service
