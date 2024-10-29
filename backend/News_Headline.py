3c40824e61f62284c68b91f2fdf31e521e8a549b
Stephen Yang <stephenyang0215@gmail.com>
2024-10-29 18:26:55 -0400

    SDD & Backend ChatGPT keywords

--- backend/News_Headline.py
+++ backend/News_Headline.py
@@ -2,25 +2,33 @@ import json
 from newsapi import NewsApiClient
 import requests
 import boto3
-from datetime import date
+from datetime import datetime
+import os
 
-today = date.today().strftime("%Y-%m-%d")
-
+news_apiKey = os.environ.get('news_apiKey')
 # Init
-newsapi = NewsApiClient(api_key='')
-session = boto3.Session(
-    aws_access_key_id='',
-    aws_secret_access_key=''
+newsapi = NewsApiClient(api_key=news_apiKey)
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+now = datetime.now()
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
+
+table = dynamodb.Table('time')
+table.put_item(
+        Item={'id':'1', 'time':dt_string}
 )
-dynamodb = session.resource('dynamodb')
+
 
 # /v2/top-headlines
 #categories: business|entertainment|general|health|science|sports|technology
 for category in ['business','entertainment','general','health','science','sports','technology']:
     print(f'Start fetching data for {category}')
-    news_get_request = requests.get(f'https://newsapi.org/v2/top-headlines?category={category}&apiKey=')
+    news_get_request = requests.get(f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={news_apiKey}')
     news_json = news_get_request.json()
-    news_json['date'] = today
+    news_json['id'] = '1'
+    news_json['date_time'] = dt_string
     #access the table in dynamoDB
     table = dynamodb.Table(f'{category}-top-headlines')
     table.put_item(
